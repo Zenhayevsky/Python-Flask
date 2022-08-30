e411a7a2d5987e1d77e8f0115a1e512b294d7aff
Zenhayevsky <zenha.solorzano.acevedo@gmail.com>
2022-08-30 15:42:58 -0300

    Nova lista usuarios com mini banco e novas rotas a linkar

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask 
+from os import abort
+
+from flask import Flask
+import db
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    html = ['<ul>']
+    for username, user in db.users.items():
+        html.append(
+                f"<li><a href='/user/{username}'>{user['name']}</a></li>"
+        )
+    html.append('</ul>')
+    return '\n'.join(html)
 
-@app.route('/hola')
-def hola():
+def profile(username):
+    user = db.users.get(username)
+    if user:
+        return f"""
+            <h1>{user['name']}</h1>
+            <img src={user['image']}/><br/> 
+            telfone: {user['tel']} <br/>
+            <a href="/">Voltar</a>       
+        """
+    else:
+        return abort(404,'user not found')
+
     return "hola"
+
+app.add_url_rule('/user/<username>/', view_func=profile, endpoint='user')
 
 @app.route('/hi')
 def hi():
     return "hi"
 
-app.run()
+app.run(use_reloader=True)
