16edd83770e63ab998f60a58d4cf9ef620d8b5f5
Zenhayevsky <zenha.solorzano.acevedo@gmail.com>
2022-08-30 15:47:47 -0300

    adicionando mini banco temporario

---
+++ db.py
@@ -0,0 +1,13 @@
+
+users = {
+    "zenha": {
+        'name': 'Zenha Acevedo',
+        'image': 'https://developer.mozilla.org/static/img/favicon144.png',
+        'tel': '11 99999999'
+    },
+    "david": {
+        "name": "David Acevedo",
+        "image": "https://almeidajunior-prod1.s3.amazonaws.com/prod/uploads/news/5d81505ac6770.jpg",
+        "tel": "11 55555555"
+    }
+}
